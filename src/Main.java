932f6f4f39d3aa565149bb4707a0f61f6cacfc40
LUM <lumlum177@gmail.com>
2022-10-18 12:10:26 +0300

    Коммит после выполнения задания

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
 
+        Product banana = new Product("Банан", 50, 5);
+        Product orange = new Product("Апельсин", 100, 4);
+        Product apple = new Product("Яблоко", 70, 3);
+        Product kiwi = new Product("Киви", 150, 2);
+        Product milk = new Product("Молоко", 80, 1);
+        Product banana2 = new Product("Бананы", 100, 5);
+
+        Set<Product> products = new HashSet<>();
+        products.add(banana);
+        products.add(orange);
+        products.add(apple);
+        products.add(kiwi);
+        products.add(milk);
+        products.add(banana2);
+
+        System.out.println(products);
+
+        products.remove(apple);
+        products.remove(orange);
+        products.remove(milk);
+        products.remove(banana2);
+
+        System.out.println(products);
     }
 }
